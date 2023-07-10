ff4b9ecee538d9bb4a6636d91255a20f8585964f
ray34-cyber <rancha.adzany1@gmail.com>
2023-07-11 05:54:45 +0700

    membuat LoginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+      return Inertia::render('login/index');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        dd('berhasil login!');
+    }
+}
